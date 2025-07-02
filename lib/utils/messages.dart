2261280a2e97d3c58ba207d751a829ca686b4e40
furqankhan76 <furqankhanfk0999@gmail.com>
2025-07-02 11:29:22 +0530

    add urdu message

--- lib/utils/messages.dart
+++ lib/utils/messages.dart
@@ -25,6 +25,14 @@ Your ward has departed from Shaheen Campus. We are pleased to share a special ph
 Warm regards,
 Shaheen Group of Institutions
 """,
+"Urdu Namaz": """
+محترم والدین،
+
+الحمدللہ، آپ کا بچہ مستقل طور پر تحریک سے جڑا ہوا ہے۔ آپ کو دل کی گہرائیوں سے مبارک ہو، اور دعا گو ہیں کہ یہ تحریک آئندہ بھی جاری رہے۔ براہ کرم خود نماز فجر میں شرکت کریں اور اپنے بچوں کو بھی بھیجتے رہیں۔
+
+گرم جوشی کے ساتھ دعا گو ہیں،
+شاہین گروپ آف انسٹی ٹیوشنز
+"""
     
     // Add more below as needed
     
